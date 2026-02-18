fa82ce4b9c476f065a3f3d28921a52dbd6b4e7f3
2SpaceMasterRace <alphanovachrono@gmail.com>
2026-02-18 13:12:08 -0500

    [Programming Languages] Finished Part 2 and Part 3 of Task 1 in Homework 1

--- CSGY 6373 - Programming Languages by Daniel Katz-Braunschweig/Practice Sets/Homework/hvarsha1.php
+++ CSGY 6373 - Programming Languages by Daniel Katz-Braunschweig/Practice Sets/Homework/hvarsha1.php
@@ -1,9 +1,25 @@
 <?php
-function simple_cipher($contents) {
-    foreach($contents as $char){
-        echo $char;
+function simple_cipher($contents, $output_file) {
+  $encrypted_text = "";
+  for($i=0; $i < strlen($contents);$i++) {
+    $char = $contents[$i];
+    if (ord($char) >= 97 && ord($char) <=122){
+        $encrypted_text .= chr((ord($char) - 97 + 13)%26 + 97);
+    }
+    else if (ord($char) >= 65 && ord($char) <=90){
+        $encrypted_text .= chr((ord($char) - 65 + 13)%26 + 65);
+    }
+    else if ($char >= 48 && $char <=57){
+        $encrypted_text .= chr((ord($char) - 48 + 5) % 10 + 48);
+    }
+    else {
+    $encrypted_text .= $char;
     }
 }
+  file_put_contents($output_file, $encrypted_text);
+}
+
+function main($argc, $argv) {
 
 /* 
 $argv[1] is the first piece of data you typed after the script name (data.txt).
@@ -14,28 +30,32 @@ This operator is a "safety shortcut." It checks two things in one go:
 
 $cipher_selection = $argv[1] ?? null;
 $input_file = $argv[2] ?? null;
+$output_file = $argv[3] ?? null;
 
-if ($argc > 3){
-    die("ERROR: Too many arguments");
+if ($argc > 4 || $argc < 4){
+    die("USAGE: php hvarsha1.php [1/2] [input file.txt] [output file.txt]");
 }
 
 if (!$input_file || pathinfo($input_file, PATHINFO_EXTENSION) !== 'txt' || !is_readable($input_file)){
-    die("ERROR: Given file is missing or not a file of format .txt");
+    die("ERROR: Given file is Missing or not a File of format .txt");
 }
-$contents = file($input_file, FILE_SKIP_EMPTY_LINES);
+
+$contents = file_get_contents($input_file);
 
 if ($contents === false){
     die("ERROR: Unable to read contents of the file");
 }
 
 if ($cipher_selection == 1){
-    simple_cipher($contents);
+    simple_cipher($contents, $output_file);
 }
 else if($cipher_selection == 2){
     die("To Be Implemented ");
 } else {
     die("ERROR: Undefined Input. Please select: \n 1 - Simple Cipher \n 2 - Ceaser's Cipher");
 }
+}
 
+main($argc, $argv);
 
 ?>
--- CSGY 6373 - Programming Languages by Daniel Katz-Braunschweig/Practice Sets/Homework/task1.md
+++ CSGY 6373 - Programming Languages by Daniel Katz-Braunschweig/Practice Sets/Homework/task1.md
@@ -5,16 +5,8 @@ Your code must meet the following restrictions
 
 1) DONE!
 
-2) The output will be the encoding of the input file.  For our purposes the algorithm is defined as follows:
-    A) If the input is a lower case letter, the output is the lower case letter plus 13 (with wrap-around, so 'n' becomes 'a')
-    B) If the input is a upper case letter, the output is the upper case letter plus 13 (with wrap-around, so 'n' becomes 'a')
-    C) If the input is a number the output is the number plus 5 (with wrap-around so '6' becomes '1')
-    D) If the input is neither letter nor number, the output is the same as the input ('!' becomes '!' and ' ' becomes ' ')
-
-3) If I execute the following (assumes 1.txt is a valid text file):
-    "php myprog.php 1 1.txt 2.txt"
-    "php myprog.php 1 2.txt 3.txt"
-   Then 1.txt and 3.txt will have the exact same information
-
-4) For this task, you may not use the str_rot13 function (that would be a little to easy).  You may define your own functions, but you do not have to.
+2) DONE!
+  
+3) DONE!
+4) DONE!
 
